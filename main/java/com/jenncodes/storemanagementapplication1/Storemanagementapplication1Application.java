1799db4f4e1becbce70224504c12da0b5a586e4e
Jenn <jenniferjuarezjj@gmail.com>
2023-10-18 22:39:16 -0400

    initial commit

---
+++ main/java/com/jenncodes/storemanagementapplication1/Storemanagementapplication1Application.java
@@ -0,0 +1,13 @@
+package com.jenncodes.storemanagementapplication1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Storemanagementapplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Storemanagementapplication1Application.class, args);
+	}
+
+}
---
+++ test/java/com/jenncodes/storemanagementapplication1/Storemanagementapplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenncodes.storemanagementapplication1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Storemanagementapplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
